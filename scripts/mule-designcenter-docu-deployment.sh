b183562accab87a75246fee270d5d4f3c96d29d1
Jose Antonio Garduno <joseantonio.garduno@ckw.ch>
2024-10-08 10:46:50 +0200

    deployment script updated

--- scripts/mule-designcenter-docu-deployment.sh
+++ scripts/mule-designcenter-docu-deployment.sh
@@ -116,10 +116,11 @@ echo "the asset will be deployt as \"$assetStatus\" and main-version \"$mainVers
 #################################################################################################
 ## LOCK THE DESIGN CENTER PROJECT MASTER BRANCH                                                ##
 #################################################################################################
+# $projectownerid" \
 httpstatus=$(curl -v \
   -H "Authorization: bearer $muleaccesstoke" \
   -H "x-organization-id: $5" \
-  -H "x-owner-id: 47c0e86c-e330-4dea-aa43-9c2f58c0ba84" \ # $projectownerid" \
+  -H "x-owner-id: 47c0e86c-e330-4dea-aa43-9c2f58c0ba84" \ 
   -X POST \
   --silent \
   --write-out %{http_code} \
@@ -132,10 +133,12 @@ httpstatus=$(curl -v \
 #################################################################################################
 ## PUBLISH THE API RAML TO EXCHANGE AS AN ASSET                                                ##
 #################################################################################################
+# $projectownerid" \
+
 publish_httpstatus=$(curl -v \
   -H "Authorization: bearer $muleaccesstoke" \
   -H "x-organization-id: $5" \
-  -H "x-owner-id: 47c0e86c-e330-4dea-aa43-9c2f58c0ba84" \ # $projectownerid" \
+  -H "x-owner-id: 47c0e86c-e330-4dea-aa43-9c2f58c0ba84" \ 
   -X POST \
   -d "{'name':'$2', 'apiVersion':'$mainVersion', 'version':'$strarr', 'main':'$4', 'assetId':'$3', 'groupId':'$5','classifier':'raml'}" \
   --silent \
@@ -150,10 +153,11 @@ jq --color-output . ./http.response.json
 #################################################################################################
 # UNLOCK THE DESIGN CENTER PROJECT MASTER BRANCH                                               ##
 #################################################################################################
+# $projectownerid" \
 httpstatus=$(curl -v \
   -H "Authorization: bearer $muleaccesstoke" \
   -H "x-organization-id: $5" \
-  -H "x-owner-id: 47c0e86c-e330-4dea-aa43-9c2f58c0ba84" \ # $projectownerid" \
+  -H "x-owner-id: 47c0e86c-e330-4dea-aa43-9c2f58c0ba84" \ 
   -X POST \
   --silent \
   --write-out %{http_code} \
