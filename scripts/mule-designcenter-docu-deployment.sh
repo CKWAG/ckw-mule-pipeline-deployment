e7cdf794fb6a84ce5450879717322dd3888a8b74
Jose Antonio Garduno <joseantonio.garduno@ckw.ch>
2024-10-09 13:15:16 +0200

    deployment script updated

--- scripts/mule-designcenter-docu-deployment.sh
+++ scripts/mule-designcenter-docu-deployment.sh
@@ -145,11 +145,13 @@ if [ $publish_httpstatus -lt 300 ];
 then
     echo "OK, HTTP-Status $publish_httpstatus";
     exit 0;
-else if [ $publish_httpstatus -eq 409 ];
+fi
+
+if [ $publish_httpstatus -eq 409 ];
 then 
     echo "WARNING $publish_httpstatus - The asset was already published as Stable"
     exit 0;
 else
-    echo "ERROR - Asset cann't be published to Exchange, error  $publish_httpstatus "
+    echo "NOK, ERROR $publish_httpstatus"
     exit 1;
 fi
